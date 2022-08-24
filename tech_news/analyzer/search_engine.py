1ef7a29b220009144f1efbf54d6c6492e3904f35
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-08-24 19:08:47 -0300

    :sparkles: feat: Implementa função search_by_title
    responsável por retornar notícias condizentes com o título pesquisado

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,13 @@
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news_by_title = search_news({"title": {"$regex": title, "$options": "i"}})
+    result_search = [
+        (new["title"], new["url"]) for new in news_by_title
+    ]
+    return result_search
 
 
 # Requisito 7
@@ -16,3 +23,7 @@ def search_by_tag(tag):
 # Requisito 9
 def search_by_category(category):
     """Seu código deve vir aqui"""
+
+
+if __name__ == "__main__":
+    print(search_by_title('Rescisão indireta:'))
