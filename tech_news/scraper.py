7f239ba9a3a0d4accba5fd8c29edfbe7cbe5b33a
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-08-24 15:02:26 -0300

    :sparkles: feat: Implementa função scrape_noticia
    responsável por recuperar dados de uma notícia

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,10 @@
 import time
 import requests
 from parsel import Selector
+from tech_news.helpers.format_scrape_noticia import (
+    format_comments_count,
+    format_summary
+)
 
 
 # Requisito 1
@@ -34,7 +38,28 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    html_selector = Selector(text=html_content)
+    url = html_selector.css('head link[rel="canonical"]::attr(href)').get()
+    title = html_selector.css(".entry-header-inner .entry-title::text").get()
+    timestamp = html_selector.css(".post-meta .meta-date::text").get()
+    writer = html_selector.css(".post-meta .meta-author .author a::text").get()
+    comments_count = html_selector.css("#comments .title-block::text").get()
+    summary = (
+        html_selector.css(".entry-content > p:first-of-type *::text").getall())
+    tags = html_selector.css(".post-tags li a::text").getall()
+    category = html_selector.css(".category-style .label::text").get()
+
+    noticia = {
+        "url": url,
+        "title": title.strip(),
+        "timestamp": timestamp,
+        "writer": writer,
+        "comments_count": format_comments_count(comments_count),
+        "summary": format_summary(summary),
+        "tags": tags,
+        "category": category,
+    }
+    return noticia
 
 
 # Requisito 5
@@ -43,5 +68,6 @@ def get_tech_news(amount):
 
 
 # if __name__ == "__main__":
-#     response = fetch("https://blog.betrybe.com/")
-#     print(scrape_next_page_link(response))
+#     url = "https://blog.betrybe.com/carreira/gatilho-mental-tudo-sobre/&#39;"
+#     response = fetch(url)
+#     print(scrape_noticia(response))
